9ff202508029de1143be912f94499f8b5a0584f2
David McGhee <davidmcg@microsoft.com>
2020-01-16 12:28:22 +1100

    updated readme to be actually useful

--- MultiICS.py
+++ MultiICS.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, Response, send_file
+#from flask import Flask, request, Response, send_file
 import requests
 import os
 import sys
-import azure.mgmt.batchai.models.azure_blob_file_system_reference
-import xmltodict, json
+#import xmltodict, json
 from lxml import etree
 
 # name the flask app - this is when I get the time to turn into a website
 #app = Flask(__name__)
 #@app.route("/", methods=['GET'])
-def GetEventsRequest():
-    """ Handles the flask request
-    """
-    nameprefix = request.args.get('namePrefix')
-    dateStart = request.args.get('nameprefix')
-    dateEnd = request.args.get('dateEnd')
+# def GetEventsRequest():
+#     """ Handles the flask request
+#     """
+#     nameprefix = request.args.get('namePrefix')
+#     dateStart = request.args.get('nameprefix')
+#     dateEnd = request.args.get('dateEnd')
 
 
 def GetMultiEventICSFile(orgIds, namePrefix, synonymPrefix, locationPrefix,dateStart, dateEnd):
@@ -49,8 +48,8 @@ def ProcessXMLintoICS(xmlstring, namePrefix, synonymPrefix, locationPrefix):
     #json.dumps(o)
     
     root = etree.fromstring(xmlstring)
-    alleventcount = int(root.xpath('count(//Event)'))
-    print("A total {0} events found for this period ".format(alleventcount))
+    alleventcount = int(root.xpath('count(/EventList/Event)'))
+    print("A total of {0} events found for this period ".format(alleventcount))
 
     if (alleventcount==0):
         print("Exiting - No events to process")
@@ -68,12 +67,11 @@ def ProcessXMLintoICS(xmlstring, namePrefix, synonymPrefix, locationPrefix):
         print("Found {0} events -- prefix {1} or synonym {2}".format(eventcount, namePrefix, synonymPrefix))
 
         if (eventcount==0):
-            print("Exiting - No named events to process")
+            print("Exiting - No named events to process -- prefix {1} or synonym {2}".format(eventcount, namePrefix, synonymPrefix))
         else:
             ProcessEventsintoICS(root, xpath, eventcount, namePrefix, synonymPrefix, locationPrefix)
 
 def ProcessEventsintoICS(root, xpath, eventcount, namePrefix, synonymPrefix, locationPrefix):
-
     # if no prefix then just look for location
     if (namePrefix):
         # also add the eventor message location if available
@@ -87,9 +85,9 @@ def ProcessEventsintoICS(root, xpath, eventcount, namePrefix, synonymPrefix, loc
 
     eventids = root.xpath(xpath)
     locCount = len(eventids)-eventcount
-
     print("Found {0} locations".format(locCount))
 
+    # create a list of event locations
     eventdata = {}
 
     # for each event then add a location based on the eventid
@@ -99,7 +97,7 @@ def ProcessEventsintoICS(root, xpath, eventcount, namePrefix, synonymPrefix, loc
             lasteventid = eventid.text
         if (eventid.tag=="Value"):
             # default is no location
-            location = "location unknonwn - was not found in eventor message"
+            location = "location unknown (not found in the eventor message)"
             # look for end of lines
             if "\n" in eventid.text:
                 # split into lines looking for location
@@ -114,15 +112,15 @@ def ProcessEventsintoICS(root, xpath, eventcount, namePrefix, synonymPrefix, loc
                     location = eventid.text.replace(locationPrefix,"")
             eventdata[lasteventid] = location    
 
-    # now the locations are extracted properly, then add it to the calendar
+    # now the locations are extracted properly add each location to an event in the calendar
     from ics import Calendar
     mainCal= None
     for key, value in eventdata.items():     
-            icsURL = "https://eventor.orienteering.asn.au/Events/ICalendar/{0}".format(key)
             # get the calendar from eventor
-            
+            icsURL = "https://eventor.orienteering.asn.au/Events/ICalendar/{0}".format(key)   
             print("Reading ICS file for event id {0}".format(key))  
             c = Calendar(requests.get(icsURL).text)
+
             # create main calendar if it doesn't exist
             if mainCal is None:
                 mainCal = c
@@ -134,6 +132,8 @@ def ProcessEventsintoICS(root, xpath, eventcount, namePrefix, synonymPrefix, loc
                     mainCal.events.add(event)
             # for the calendar to be used it must be published
             c.method = "PUBLISH"    
+            # TODO: add in  X-WR-CALNAME e.g. "Sydney Summer Series Season 29 "Map Running Sydney"
+
 
     print("Writing combined {0} events".format(len(mainCal.events)))    
 
@@ -155,7 +155,7 @@ if __name__ == '__main__':
 
     import argparse
 
-    parser = argparse.ArgumentParser(description='Arguments for event caldendar')
+    parser = argparse.ArgumentParser(description='Arguments for eventor caldendar')
 
     parser.add_argument('-o', '--organisationIds', action="store", dest="orgIds", help='comma separated list fo organisationIds name for event')
     parser.add_argument('-p', '--prefix', action="store", dest='namePrefix', help='prefix of event name')
--- readme.md
+++ readme.md
@@ -1,6 +1,32 @@
 
+# Usage
+
+I havent packaged this up yet so its just some plain old python code you can run from the directory / command line (assumign you have python tooling)
+
+Location as a field is a pain to extract. Orienteering event creators using Eventor often the event location them in a special Evenot message area (a blue box on the web site).
+The location is found by looking for a key word e.g. "Location: The Park"
+
+Example below takes the Newcastle organisation (29) and sepcifies the prefix "Location:" and sets a date start and date end
+```
+Python
+MultiICS.py' '--organisationIds' '29' '--location' 'Location: ' '2019-10-01' '2020-03-31'
+```
+
+Example below looks for the prefix "Bay Orienteering Series" or a synonym "River Orienteering Series" and specifies the prefix "Venue:" and sets a date start and date end
+```
+Python
+MultiICS.py' 'Bay Orienteering Series' '--synonym' 'River Orienteering Series' '--location' 'Venue: ' '2019-10-13' '2019-12-15'
+```
+
+Example below looks for the prefix "Sydney Summer" or a synonym "SSS" sets a date start and date end
+```
+Python
+MultiICS.py' 'Sydney Summer' '-s' 'SSS' '2019-10-01' '2020-04-01' 
+```
+See results of these queries in this [directory](./docs/examples)
 
 
-Should this be a web app - where you put in the search
-Should this be an api - you put in some parameters on the query string
+# Comments
+This was originally designed as a web app - where you put in the search on a form....
+If I were building this for an enteprrise  I would make it an API for people to use  but this is probably just ok for its occasional usage
 
